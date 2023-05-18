5bd092102ae3cd05a8ba4b8e0719750b6a260a63
Michael Lombardi <mlombardi@microsoft.com>
2023-05-17 23:12:45 -0500

    (MAINT) Add handling to fix TOC auto expand/collapse
    This change adds the new `stopPropagationInTree` async function to the Platen
    module and uses it in the new tocScript partial to ensure that selecting a
    link in the TOC does not automatically expand or collapse that tree item.
    
    This makes the behavior of the TOC more predictable and provides a built-in
    way for other components to borrow this same behavior as needed.

--- modules/platen/assets/scripts/platen/module.js
+++ modules/platen/assets/scripts/platen/module.js
@@ -49,6 +49,39 @@ export function toggleTableOfContents() {
   }
 }
 
+/**
+ * The `stopPropagationInTree` function is used to keep intentional interactive events in a
+ * Shoelace tree-item from bubbling up and causing the parent tree item to collapse or expand.
+ * 
+ * This is particularly useful for trees that include several interactive objects, like the trees
+ * for the Table of Contents links.
+ * 
+ * This function must be called once against each tree you want to prevent propagation in. Every
+ * found element matching the targetSelector in that tree is set to stop the event propagation for
+ * events with the specified type.
+ * 
+ * To stop propagation for multiple events or trees, call the function for each event and tree.
+ * 
+ * @param {string} targetSelector The CSS selector string for the interactive object to target.
+ * @param {string} treeSelector The CSS selector string for the Shoelace tree the target is in.
+ * @param {string} eventType  The type of event to stop propagation for.
+ */
+export async function stopPropagationInTree(targetSelector, treeSelector = 'sl-tree', eventType = 'click') {
+  customElements.whenDefined('sl-tree').then(async () => {
+    const tree = document.querySelector(treeSelector)
+
+    if (tree != undefined) await tree.updateComplete;
+
+    document.querySelectorAll(targetSelector).forEach(element => {
+      element.addEventListener(
+        eventType,
+        event => { event.stopPropagation() },
+        true
+      )
+    })
+  })
+}
+
 export const Version = '0.1.0'
 
 export default Version
